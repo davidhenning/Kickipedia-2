276393936c3dae9ae21a2e2eec24f64b3731cb1a
David Henning <madcat.me@gmail.com>
2012-04-17 20:29:41 +0200

    added new class Base

---
+++ MongoAppKit/Base.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace MongoAppKit;
+
+abstract class Base {
+
+    private $_oConfig = null;
+    private $_oStorage = null;
+    private $_oSession = null;
+
+    public function getConfig() {
+        if($this->_oConfig === null) {
+            $this->_oConfig = new Config();
+        }
+
+        return $this->_oConfig;
+    }
+
+    public function getStorage() {
+        if($this->_oStorage === null) {
+            $this->_oStorage = new Storage($this->getConfig());
+        }
+
+        return $this->_oStorage;       
+    }
+
+    public function getSession() {
+        if($this->_oSession === null) {
+            $this->_oSession = new Storage($this->getConfig());
+        }
+
+        return $this->_oSession;       
+    }
+}
