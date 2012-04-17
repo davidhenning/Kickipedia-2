71a614f1045a4259979fe834859006a302935c0c
David Henning <madcat.me@gmail.com>
2012-04-17 21:02:36 +0200

    show page generation time

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+$time = microtime(true);
+
 require_once(__DIR__ . '/bootstrap.php');
 
 use Kickipedia2\Controllers\EntryActions;
@@ -7,3 +9,5 @@ use Kickipedia2\Controllers\EntryActions;
 $entryActions = new EntryActions();
 
 run();
+
+echo sprintf("%01.6f", microtime(true) - $time).'s';
