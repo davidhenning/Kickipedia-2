086e110f3d642d0805e231a09dc596a8021cc7b6
David Henning <madcat.me@gmail.com>
2012-04-17 20:29:28 +0200

    moved Constructor and Methods getId() and setId() into class View

--- Kickipedia2/Views/EntryView.php
+++ Kickipedia2/Views/EntryView.php
@@ -10,20 +10,6 @@ class EntryView extends View {
     protected $_sTemplateName = 'entry.twig';
     protected $_sId = null;
 
-    public function __construct($id = null) {
-        if($id !== null) {
-            $this->setId($id);
-        }
-    }
-
-    public function getId() {
-        return $this->_sId;
-    }
-
-    public function setId($id) {
-        $this->_sId = $id;
-    }
-
     public function render() {
         $entry = new EntryModel();
         $entry->load($this->getId());  
--- MongoAppKit/View.php
+++ MongoAppKit/View.php
@@ -6,6 +6,21 @@ abstract class View {
     
     protected $_sTemplateName = null;
     protected $_aTemplateData = array();
+    protected $_sId = null;
+
+    public function __construct($id = null) {
+        if($id !== null) {
+            $this->setId($id);
+        }
+    }
+
+    public function getId() {
+        return $this->_sId;
+    }
+
+    public function setId($id) {
+        $this->_sId = $id;
+    }
 
     public function render() {
         $loader = new \Twig_Loader_Filesystem(getBasePath() .'\Kickipedia2\Templates');
