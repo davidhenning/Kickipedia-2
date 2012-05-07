06a5af4a081d3c5df44ee5c1463b1bcfe23ec23f
David Henning <madcat.me@gmail.com>
2012-05-07 21:25:30 +0200

    STYLE! (almost :D)

--- public/assets/config.rb
+++ public/assets/config.rb
@@ -5,10 +5,10 @@ http_path = "/"
 css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
-javascripts_dir = "javascripts"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
