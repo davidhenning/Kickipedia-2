e87aa4e6fb1aa91631cfd9fee3b3f7dab73ca5ce
dhenning <dhenning@chip.de>
2012-05-10 08:36:08 +0200

    small script to fill demo entries into mongodb

---
+++ fill.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once(__DIR__ . './bootstrap.php');
+
+use Kickipedia2\Models\EntryRecord;
+
+for($i = 0; $i < 1000; $i++) {
+	$entry = new EntryRecord();
+	$entry->setProperty('type', 3);
+	$entry->setProperty('user', 1);
+	$entry->setProperty('name', "Victim {$i}");
+	$entry->setProperty('reason', 'Troll');
+	$entry->setProperty('ip', '127.0.0.1');
+	$entry->save();
+}
