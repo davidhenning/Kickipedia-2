260cb5ca158170a511e4bec9dc95ad3fea57d06f
David Henning <madcat.me@gmail.com>
2012-05-17 22:03:26 +0200

    added HTTP digest auth

--- public/index.php
+++ public/index.php
@@ -5,6 +5,31 @@ $time = microtime(true);
 require_once(__DIR__ . '/../bootstrap.php');
 
 use Kickipedia2\Controllers\EntryActions;
+use MongoAppKit\HttpAuthDigest;
+use MongoAppKit\Exceptions\HttpException;
+
+try {
+    $oAuth = new HttpAuthDigest('Kickipedia2', $_SERVER['PHP_AUTH_DIGEST']);
+    $oAuth->sendAuthenticationHeader();
+
+    $aUsers = array(
+        'MadCat' => md5("MadCat:Kickipedia2:test"),
+        'kicki' => md5("kicki:Kickipedia2:pedia")
+    );
+
+    $sUser = $oAuth->getUserName();
+
+    $oAuth->authenticate($aUsers[$sUser]);
+} catch(HttpException $e) {
+    if($e->getCode() === 401) {
+        $oAuth->sendAuthenticationHeader(true);
+    }
+
+    echo $e->getMessage();
+    exit();
+}
+
+
 
 $entryActions = new EntryActions();
 
