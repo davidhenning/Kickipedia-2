579519557b9b8eaae86572197f6b75dee6b631c3
David Henning <madcat.me@gmail.com>
2012-06-07 18:11:08 +0200

    use Phpass to set password

--- Kickipedia2/Models/UserDocument.php
+++ Kickipedia2/Models/UserDocument.php
@@ -2,11 +2,28 @@
 
 namespace Kickipedia2\Models;
 
+use \Phpass\Hash,
+    \Phpass\Hash\Adapter\Pbkdf2;
+
 class UserDocument extends BaseDocument {
     
     public function __construct() {
         parent::__construct();
 
         $this->setCollectionName('user');
+    }
+
+    public function setPassword($sPassword) {
+        try {
+            $oAdapter = new Pbkdf2(array (
+                'iterationCountLog2' => 16
+            ));
+
+            $oHash = new Hash($oAdapter);
+            $sHash = $oHash->hashPassword($sPassword);
+            $this->setProperty('password', $sHash);
+        } catch(\Exception $e) {
+
+        }
     }
 }
