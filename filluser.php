231195480c73a3d490b25cd102e27f5950c3335d
David Henning <madcat.me@gmail.com>
2012-06-07 18:11:17 +0200

    use setPassword

--- filluser.php
+++ filluser.php
@@ -6,14 +6,14 @@ use Kickipedia2\Models\UserDocument;
 
 $entry = new UserDocument();
 $entry->setProperty('name', 'MadCat');
-$entry->setProperty('password', md5('test'));
+$entry->setPassword('test');
 $entry->setProperty('token', md5("MadCat:Kickipedia2:test"));
 $entry->setProperty('email', 'madcat.me@gmail.com');
 $entry->save();
 
 $entry = new UserDocument();
 $entry->setProperty('name', 'kicki');
-$entry->setProperty('password', md5('pedia'));
+$entry->setPassword('pedia');
 $entry->setProperty('token', md5("kicki:Kickipedia2:pedia"));
 $entry->setProperty('email', 'madcat.me@gmail.com');
 $entry->save();
