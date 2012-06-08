6ed9ad5887a7673ab8e2778c4a1ca9914bea831f
David Henning <madcat.me@gmail.com>
2012-06-08 16:14:15 +0200

    create random ip address

--- fill.php
+++ fill.php
@@ -6,10 +6,12 @@ use Kickipedia2\Models\EntryDocument;
 
 for($i = 0; $i < 10000; $i++) {
 	$entry = new EntryDocument();
+	$ip = rand(1, 255).'.'.rand(1, 255).'.'.rand(1,255).'.'.rand(1,255);
+
 	$entry->setProperty('type', rand(1,4));
 	$entry->setProperty('user', 1);
 	$entry->setProperty('name', "Victim {$i}");
 	$entry->setProperty('reason', 'Troll');
-	$entry->setProperty('ip', '127.0.0.1');
+	$entry->setProperty('ip', $ip);
 	$entry->save();
 }
