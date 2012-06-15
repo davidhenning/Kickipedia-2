783575c925ba0ad696835c894b77ebcfb6f6bf20
David Henning <madcat.me@gmail.com>
2012-06-16 01:28:04 +0200

    send JSON error message on HTTP 400 with JSON payload

--- public/error.php
+++ public/error.php
@@ -1,3 +1,32 @@
+<?php
+
+if($e->getCode() === 400) {
+    header('HTTP/1.0 400 Bad Request', true, 400);
+
+    if($_SERVER['CONTENT_TYPE'] === 'application/json') {
+        header('Content-type: application/json');
+        header('HTTP/1.0 400 Bad Request', true, 400);
+
+        $aError = array(
+            'status' => 400,
+            'time' => date('Y-m-d H:i:s'),
+            'request' => array(
+                'method' => 'PUT',
+                'url' => request_uri()
+            ),
+            'response' => array(
+                'error' => str_ireplace('exception', '', get_class($e)),
+                'message' => $e->getMessage(),
+                'backtrace' => $e->getTrace()
+            )
+        );
+
+        echo json_encode($aError);
+        exit();
+    }
+}
+
+?>
 <!DOCTYPE html>
 <html>
     <head>
