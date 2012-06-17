34886b81d8120037465224558ee09dff9c3ca40e
David Henning <madcat.me@gmail.com>
2012-06-17 17:27:05 +0200

    XML output for single entry view

--- Kickipedia2/Views/EntryView.php
+++ Kickipedia2/Views/EntryView.php
@@ -21,7 +21,7 @@ class EntryView extends BaseView {
     protected function _renderJSON() {
         $oEntry = new EntryDocument();
         $oEntry->load($this->getId());
-        
+
         $aOutput = array(
             'status' => 200,
             'time' => date('Y-m-d H:i:s'),
@@ -39,5 +39,32 @@ class EntryView extends BaseView {
 		$aOutput['response']['documents'][] = $oEntry->getProperties();
 
         echo json_encode($aOutput);
+    }
+
+    protected function _renderXML() {
+        $oEntry = new EntryDocument();
+        $oEntry->load($this->getId());
+        $oKickipedia = new \SimpleXMLElement('<kickipedia></kickipedia>');
+        
+        $oKickipedia->addChild('status', 200);
+        $oKickipedia->addChild('date', date('Y-m-d H:i:s'));
+        
+        $oRequest = $oKickipedia->addChild('request');
+        $oRequest->addChild('method', 'GET');
+        $oRequest->addChild('url', request_uri());
+
+        $oResponse = $oKickipedia->addChild('response');
+        $oResponse->addChild('total', 1);
+        $oResponse->addChild('found', 1);
+
+        $oDocuments = $oResponse->addChild('documents');
+
+
+        $oDocument = $oDocuments->addChild('document');
+        foreach($oEntry->getProperties() as $key => $value) {
+            $oDocument->addChild($key, $oEntry->getProperty($key));
+        }
+
+        echo $oKickipedia->asXML();
     }
 }
