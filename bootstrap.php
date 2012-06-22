40b0e5fa1979a41b85cd5b83deda7884b43e3ac1
David Henning <madcat.me@gmail.com>
2012-06-22 23:11:30 +0200

    MongoAppKit is a separate GitHub repository with Composer support

--- bootstrap.php
+++ bootstrap.php
@@ -7,8 +7,6 @@ if(!function_exists('getBasePath')) {
 }
 
 require_once(__DIR__ . '/vendor/autoload.php');
-require_once(__DIR__ . '/MongoAppKit/Loader.php');
 require_once(__DIR__ . '/Kickipedia2/Loader.php');
 
-\MongoAppKit\Loader::registerAutoloader();
 \Kickipedia2\Loader::registerAutoloader();
