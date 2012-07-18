7ad132c5bd867ab32aeb0b88d7aa1e90a94d724e
David Henning <madcat.me@gmail.com>
2012-07-18 22:37:22 +0200

    logical order

--- public/assets/js/hook.js
+++ public/assets/js/hook.js
@@ -3,8 +3,8 @@
 
     var requireConfig = {
             paths: {
-                'jquery': 'vendor/jquery/1.7.2',
                 'moduleloader': 'core/moduleloader',
+                'jquery': 'vendor/jquery/1.7.2',             
                 'jqhttp': 'vendor/jquery-plugins/http-1.0.0',
                 'tablesorter': 'vendor/jquery-plugins/tablesorter-2.0.5'
             }
