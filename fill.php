c3f1fc31a4f9ae93636c1ea2af8497467bb85497
David Henning <madcat.me@gmail.com>
2012-08-24 21:22:29 +0200

    fix

--- fill.php
+++ fill.php
@@ -7,14 +7,21 @@ use MongoAppKit\Config,
 
 use Kickipedia2\Models\EntryDocument;
 
+use Silex\Application;
+
+$oApp = new Application();
 $oConfig = new Config();
-$oConfig->addConfigFile('mongoappkit.json');
-$oConfig->addConfigFile('kickipedia2.json');
+$oConfig->setBaseDir(realpath(__DIR__));
+$oConfig->addConfigFile($oConfig->getConfDir() . '/mongoappkit.json');
+$oConfig->addConfigFile($oConfig->getConfDir() . '/kickipedia2.json');
 $oStorage = new Storage($oConfig);
-$oDatabase = $oStorage->getDatabase();
+
+$oApp['debug'] = $oConfig->getProperty('DebugMode');
+$oApp['config'] = $oConfig;
+$oApp['storage'] = $oStorage;
 
 for($i = 0; $i < 10000; $i++) {
-	$entry = new EntryDocument($oDatabase, $oConfig);
+	$entry = new EntryDocument($oApp);
 	$ip = rand(1, 255).'.'.rand(1, 255).'.'.rand(1,255).'.'.rand(1,255);
 
 	$entry->setProperty('type', rand(1,4));
--- filluser.php
+++ filluser.php
@@ -7,20 +7,27 @@ use MongoAppKit\Config,
 
 use Kickipedia2\Models\UserDocument;
 
-$oConfig = new Config();
-$oConfig->addConfigFile('mongoappkit.json');
-$oConfig->addConfigFile('kickipedia2.json');
-$oStorage = new Storage($oConfig);
-$oDatabase = $oStorage->getDatabase();
+use Silex\Application;
 
-$entry = new UserDocument($oDatabase, $oConfig);
+$oApp = new Application();
+$oConfig = new Config();
+$oConfig->setBaseDir(realpath(__DIR__));
+$oConfig->addConfigFile($oConfig->getConfDir() . '/mongoappkit.json');
+$oConfig->addConfigFile($oConfig->getConfDir() . '/kickipedia2.json');
+$oStorage = new Storage($oConfig);
+
+$oApp['debug'] = $oConfig->getProperty('DebugMode');
+$oApp['config'] = $oConfig;
+$oApp['storage'] = $oStorage;
+
+$entry = new UserDocument($oApp);
 $entry->setProperty('name', 'MadCat');
 $entry->setPassword('test');
 $entry->setProperty('token', md5("MadCat:Kickipedia2:test"));
 $entry->setProperty('email', 'madcat.me@gmail.com');
 $entry->save();
 
-$entry = new UserDocument($oDatabase, $oConfig);
+$entry = new UserDocument($oApp);
 $entry->setProperty('name', 'kicki');
 $entry->setPassword('pedia');
 $entry->setProperty('token', md5("kicki:Kickipedia2:pedia"));
