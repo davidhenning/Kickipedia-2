8dc314018dcec653fd45c58330a44169401ce41f
David Henning <madcat.me@gmail.com>
2012-09-13 21:18:31 +0200

    movd complete application logic into class Application

--- index.php
+++ index.php
@@ -7,96 +7,22 @@ $sMainPath = realpath(__DIR__);
 
 require_once(realpath($sMainPath . '/bootstrap.php'));
 
+use Symfony\Component\HttpFoundation\Request;
+
 use MongoAppKit\Config,
-    MongoAppKit\Storage,
-    MongoAppKit\HttpAuthDigest, 
-    MongoAppKit\Exceptions\HttpException;
+    MongoAppKit\Storage;
 
-use Kickipedia2\Controllers\EntryActions,
-    Kickipedia2\Models\UserDocumentList;
+use Kickipedia2\Application,
+    Kickipedia2\Controllers\EntryActions;
 
-use Symfony\Component\HttpFoundation\Request,
-    Symfony\Component\HttpFoundation\Response,
-    Symfony\Component\HttpKernel\Debug\ExceptionHandler;
-
-use Kickipedia2\Application;
-
-use Monolog\Logger,
-    Monolog\Handler\StreamHandler;
+Request::trustProxyData();
 
 $oConfig = new Config();
 $oConfig->setBaseDir(realpath($sMainPath));
 $oConfig->addConfigFile($oConfig->getConfDir() . '/mongoappkit.json');
 $oConfig->addConfigFile($oConfig->getConfDir() . '/kickipedia2.json');
 $oStorage = new Storage($oConfig);
-Request::trustProxyData();
 
 $oApp = new Application($oConfig, $oStorage);
-
-$oApp->before(function(Request $oRequest) use($oConfig) {
-    if(strpos($oRequest->headers->get('Content-Type'), 'application/json') === 0) {
-        $aData = json_decode($oRequest->getContent(), true);
-        $oRequest->request->replace(is_array($aData) ? $aData : array());
-    }
-});
-
-$oApp->before(function(Request $oRequest) use($oApp, $oConfig) {      
-    $oAuth = new HttpAuthDigest($oRequest, 'Kickipedia2'); 
-    $oResponse = $oAuth->sendAuthenticationHeader();
-
-    if($oResponse instanceof Response) {
-        return $oResponse;
-    }
-
-    $oUserList = new UserDocumentList($oApp);
-    $sUserName = $oConfig->sanitize($oAuth->getUserName());
-    $aUserDocument = $oUserList->getUser($sUserName);
-    $_SESSION['user'] = $aUserDocument;
-
-    $oAuth->authenticate($aUserDocument->getProperty('token'));
-});
-
-$oApp->error(function(HttpException $e, $code) use($oApp) {
-    if($e->getCode() === 401) {
-        return $e->getCallingObject()->sendAuthenticationHeader(true);
-    }
-
-    $oExceptionHandler = new ExceptionHandler($oApp['config']);
-    
-    return $oExceptionHandler->createResponse($e);
-});
-
-$oApp->error(function(Exception $e, $code) use($oApp) {
-    $oRequest = $oApp['request'];
-
-    $oLog = new Logger('name');
-    $oLog->pushHandler(new StreamHandler(getBasePath() . 'logs/error.log', Logger::WARNING));
-
-    $aError = array(
-        'status' => $e->getCode(),
-        'time' => date('Y-m-d H:i:s'),
-        'request' => array(
-            'method' => $oRequest->getMethod(),
-            'url' => $oRequest->getPathInfo()
-        ),
-        'response' => array(
-            'error' => str_ireplace('exception', '', get_class($e)),
-            'message' => $e->getMessage(),
-            'backtrace' => $e->getTrace(),
-            'server' => $_SERVER
-        )
-    );
-
-    $oLog->addError("{$oRequest->getMethod()} {$oRequest->getPathInfo()} - HTTP response {$e->getCode()}: {$e->getMessage()}");
-
-    if(strpos($oRequest->headers->get('Content-Type'), 'application/json') === 0) {
-        return $oApp->json($aError, 400);
-    }
-
-    $oExceptionHandler = new ExceptionHandler($oApp['config']);
-
-    return $oExceptionHandler->createResponse($e);
-});
-
-$entryActions = new EntryActions($oApp);
+$oApp->mount('', new EntryActions());
 $oApp->run();
--- src/Kickipedia2/Application.php
+++ src/Kickipedia2/Application.php
@@ -4,16 +4,94 @@ namespace Kickipedia2;
 
 use Silex\Application as SilexApplication;
 
+use Symfony\Component\HttpFoundation\Request,
+    Symfony\Component\HttpFoundation\Response,
+    Symfony\Component\HttpKernel\Debug\ExceptionHandler;
+
 use MongoAppKit\Config,
-    MongoAppKit\Storage;
+    MongoAppKit\Storage,
+    MongoAppKit\HttpAuthDigest, 
+    MongoAppKit\Exceptions\HttpException;
+
+use Kickipedia2\Models\UserDocumentList;
+
+use Monolog\Logger,
+    Monolog\Handler\StreamHandler;
 
 class Application extends SilexApplication {
 	
 	public function __construct(Config $oConfig, Storage $oStorage) {
 		parent::__construct();
 
+		$oApp = $this;
+
 		$this['debug'] = $oConfig->getProperty('DebugMode');
 		$this['config'] = $oConfig;
 		$this['storage'] = $oStorage;
+
+		$this->before(function(Request $oRequest) use($oConfig) {
+		    if(strpos($oRequest->headers->get('Content-Type'), 'application/json') === 0) {
+		        $aData = json_decode($oRequest->getContent(), true);
+		        $oRequest->request->replace(is_array($aData) ? $aData : array());
+		    }
+		});
+
+		$this->before(function(Request $oRequest) use($oApp, $oConfig) {      
+		    $oAuth = new HttpAuthDigest($oRequest, 'Kickipedia2'); 
+		    $oResponse = $oAuth->sendAuthenticationHeader();
+
+		    if($oResponse instanceof Response) {
+		        return $oResponse;
+		    }
+
+		    $oUserList = new UserDocumentList($oApp);
+		    $sUserName = $oConfig->sanitize($oAuth->getUserName());
+		    $aUserDocument = $oUserList->getUser($sUserName);
+		    $_SESSION['user'] = $aUserDocument;
+
+		    $oAuth->authenticate($aUserDocument->getProperty('token'));
+		});
+
+		$this->error(function(HttpException $e, $code) use($oApp) {
+		    if($e->getCode() === 401) {
+		        return $e->getCallingObject()->sendAuthenticationHeader(true);
+		    }
+
+		    $oExceptionHandler = new ExceptionHandler($oApp['config']);
+		    
+		    return $oExceptionHandler->createResponse($e);
+		});
+
+		$this->error(function(\Exception $e, $code) use($oApp) {
+		    $oRequest = $oApp['request'];
+
+		    $oLog = new Logger('name');
+		    $oLog->pushHandler(new StreamHandler(getBasePath() . 'logs/error.log', Logger::WARNING));
+
+		    $aError = array(
+		        'status' => $e->getCode(),
+		        'time' => date('Y-m-d H:i:s'),
+		        'request' => array(
+		            'method' => $oRequest->getMethod(),
+		            'url' => $oRequest->getPathInfo()
+		        ),
+		        'response' => array(
+		            'error' => str_ireplace('exception', '', get_class($e)),
+		            'message' => $e->getMessage(),
+		            'backtrace' => $e->getTrace(),
+		            'server' => $_SERVER
+		        )
+		    );
+
+		    $oLog->addError("{$oRequest->getMethod()} {$oRequest->getPathInfo()} - HTTP response {$e->getCode()}: {$e->getMessage()}");
+
+		    if(strpos($oRequest->headers->get('Content-Type'), 'application/json') === 0) {
+		        return $oApp->json($aError, 400);
+		    }
+
+		    $oExceptionHandler = new ExceptionHandler($oApp['config']);
+
+		    return $oExceptionHandler->createResponse($e);
+		});
 	}
 }
